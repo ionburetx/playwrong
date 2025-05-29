7bdb4792cb1b7c38458291d65dcc861697114f75
ionburetx <ionburgoa1@gmail.com>
2025-05-29 10:49:07 +0200

    transicion de splash a home

--- src/router.jsx
+++ src/router.jsx
@@ -6,13 +6,15 @@ import Genre from './pages/Genre';
 import ProtectedRoute from "./components/ProtectedRoute";
 import MyProfile from "./pages/MyProfile";
 import Search from './pages/Search';
+import Splash from "./pages/splash";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Splash />} />
         <Route element={<App />}>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/movie/:id" element={<Details />} />
           <Route path="/genre/:genreId" element={<Genre />} />
           <Route path="/myprofile" element={<ProtectedRoute><MyProfile /></ProtectedRoute>} />
