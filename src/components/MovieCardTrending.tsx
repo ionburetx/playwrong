50d2b47fbc1918b40024d36e0e95fe0cab12d350
portegag2 <portegag2@gmail.com>
2025-06-02 00:54:35 +0200

    TrendingList, moviecartrending mejor alineamiento

--- src/components/MovieCardTrending.tsx
+++ src/components/MovieCardTrending.tsx
@@ -24,16 +24,16 @@ const MovieCardTrending: FC<MovieCardTrendingProps> = ({ movie, isTrending }) =>
         alt={movie.title}
         className="w-full h-full object-cover"
       />
-      <div className="absolute inset-0 flex items-center bg-gradient-to-r from-black to-transparent">
-        <div className="p-4 md:p-8 ml-4 md:ml-10 max-w-2xl">
-          <h3 className="md:text-4xl font-bold text-white mb-4 ml-4 md:ml-10 opacity-0 animate-[fadeIn_1s_ease-out_0.5s_forwards]">{movie.title}</h3>
+      <div className="absolute inset-0 flex items-center md:items-start bg-gradient-to-r from-black to-transparent">
+        <div className="p-4 ml-4 md:p-8 md:pl-0 md:ml-16 max-w-2xl md:mt-[35vh]">
+          <h3 className="md:text-4xl font-bold text-white mb-4 ml-4 md:ml-4 opacity-0 animate-[fadeIn_1s_ease-out_0.5s_forwards]">{movie.title}</h3>
           {isTrending && (
-            <p className="text-white text-sm md:text-lg mb-6 ml-4 md:ml-10 mr-4 md:mr-0 opacity-0 animate-[fadeIn_1s_ease-out_0.7s_forwards] line-clamp-4 md:line-clamp-none">{movie.overview}</p>
+            <p className="text-white text-sm md:text-lg mb-6 ml-4 mr-4 md:mx-4 opacity-0 animate-[fadeIn_1s_ease-out_0.7s_forwards] line-clamp-4 md:line-clamp-none">{movie.overview}</p>
           )}
           <Link
             to={`/movie/${movie.id}`}
             className="inline-block px-6 py-3 bg-blue-600 text-white rounded-md 
-              hover:bg-blue-700 transition-colors ml-4 md:ml-10 mb-12 opacity-0 animate-[fadeIn_1s_ease-out_0.7s_forwards]"
+              hover:bg-blue-700 transition-colors ml-4 md:ml-4 mb-12 opacity-0 animate-[fadeIn_1s_ease-out_0.7s_forwards]"
           >
             Ver más
           </Link>
